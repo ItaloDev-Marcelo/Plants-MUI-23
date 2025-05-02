b00f6af4eb7b7a027b9e9e0903a605aa1528cf9a
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-05-02 10:38:48 -0300

    update

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -2,13 +2,14 @@ import {Stack, Typography} from '@mui/material'
 
 export default function Header() {
     return (
-      <Stack component='header' id='home-information' padding={{xs: '2em'}}>
-         <Typography variant='h1' fontSize={{xs: '2em'}} marginY={1} maxWidth={250} >
+      <Stack component='header' id='home-information' padding={{xs: ' .5em 2em'}}>
+         <Typography variant='h1' fontSize={{xs: '2.5em', lg: '3em'}} marginY={1} maxWidth={225} >
              <span id='green'>Go Green</span> <br/>
          <span id='c-2'> The world of plants</span></Typography>
-         <Typography variant='p' maxWidth={300}  fontSize={{xs: '.7em'}} marginY={1}>Discover everything you need to know about your plants, 
+         <Typography variant='p' maxWidth={300}  fontSize={{xs: '1em'}} marginY={1}>Discover everything you need to know about your plants, 
             treat them with kindness and they will take care of you.</Typography>
-         <Typography variant='h2' fontSize={{xs: '1.2em'}}>Top 5 of the week
+         <Typography variant='h2' marginY={1} fontSize={{xs: '1.2em', lg: '1.7em'}} fontWeight='bold'>
+           Top 5 of the week
          </Typography>
       </Stack>
     )
