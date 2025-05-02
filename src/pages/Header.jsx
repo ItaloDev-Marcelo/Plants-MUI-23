caa46c459a7520769e841d34374437f9f81e9a77
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-05-02 11:37:37 -0300

    up

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
          <Typography variant='h1' fontSize={{xs: '2.5em', lg: '3em'}} marginY={1} maxWidth={225} >
              <span id='green'>Go Green</span> <br/>
          <span id='c-2'> The world of plants</span></Typography>
-         <Typography variant='p' maxWidth={300}  fontSize={{xs: '1em'}} marginY={1}>Discover everything you need to know about your plants, 
+         <Typography variant='p' maxWidth={{xs: 300, lg: 500}}  fontSize={{xs: '1em'}} marginY={1}>Discover everything you need to know about your plants, 
             treat them with kindness and they will take care of you.</Typography>
          <Typography variant='h2' marginY={1} fontSize={{xs: '1.2em', lg: '1.7em'}} fontWeight='bold'>
            Top 5 of the week
